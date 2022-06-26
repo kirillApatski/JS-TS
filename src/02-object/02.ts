289d74f8e3100b21c36fd3c4b20ad461f550ea24
Kirill <apatski94@gmail.com>
2022-06-26 22:30:56 +0300

    add directory 02-object 02-test file 02.ts file 02-02.ts

---
+++ src/02-object/02.ts
@@ -0,0 +1,51 @@
+type CityType = {
+    title: string
+    countryTitle: string
+}
+
+type AddressType = {
+    streetTitle: string
+    city: CityType
+}
+
+type TechnologiesType = {
+    id: number
+    title: string
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechnologiesType>
+}
+
+export const student: StudentType = {
+    id: 1,
+    name: "Kirill",
+    age: 28,
+    isActive: false,
+    address: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: "Minsk",
+            countryTitle: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "React"
+        }
+    ]
+}
